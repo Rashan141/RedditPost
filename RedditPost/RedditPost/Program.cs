79c9bfb95d473efa2c92c279c2cc6caacc3a507b
Rashan Whittaker <Rashan141@users.noreply.github.com>
2018-05-14 16:14:13 -0500

    Reddit Post logic

---
+++ RedditPost/RedditPost/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RrditPost> allPosts = new List<RrditPost>();
+
+            Console.WriteLine("Please enter your Username and post your message.");
+
+            var input = Console.ReadLine();
+
+            while (input != "done") {
+                
+                RrditPost user = new RrditPost(input);
+
+                Console.WriteLine("Write your post: \n");
+                input = Console.ReadLine();
+
+                user.Post = input;
+
+                allPosts.Add(user);
+
+                        foreach (var item in allPosts)
+                        {
+                            PostContainer(item);
+                        }
+
+                Console.WriteLine("If you are finished, type \"done\"");
+                input = Console.ReadLine();
+            }
+        }
+
+        public static void PostContainer(RrditPost message) {
+
+            Console.WriteLine("{0}-------------------------------------------------------------------------------+", message.GetUserName());
+            Console.WriteLine("{0}", message.Post);
+            Console.WriteLine("\n");
+            Console.WriteLine("+ Votes:{0}--------------------------------------------------------------------------------+", message.GetVotes());
+
+        }
+    }
+}
---
+++ RedditPost/RedditPost/RrditPost.cs
@@ -0,0 +1,37 @@
+﻿namespace RedditPost
+{
+    class RrditPost {
+
+        private string _username;
+        private int _votes;
+        private string _post = "Empty text.";
+
+        public string Post {
+            get { return _post; }
+
+            set { _post = value; }
+        }
+
+
+        public RrditPost(string alias)
+        {
+            _username = alias;
+        }
+
+        public void Upvote() {
+            _votes++;
+        }
+
+        public void Downvote() {
+            _votes--;
+        }
+
+        public int GetVotes() {
+            return _votes;
+        }
+
+        public string GetUserName() {
+            return _username;
+        }
+    }
+}
